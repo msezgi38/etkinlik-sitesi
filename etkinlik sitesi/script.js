0c8a31137bbad4ae791c750cb00195ecd59a4722
Mustafa Sezgi <76861453+msezgi38@users.noreply.github.com>
2022-06-09 12:16:53 +0300

    Add files via upload

---
+++ etkinlik sitesi/script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+var swiper = new Swiper(".review-slider", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1050: {
+      slidesPerView: 3,
+    },
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  }
+});
